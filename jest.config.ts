64ee3c2b1c83c4313d66ebd561a99b0d8fa1be69
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2022-06-16 15:57:32 -0300

    test(jest.config.ts): configurada cobertura

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   transform: {
-    // '\\.tsx$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     "^.+\\.(js|jsx)$": "babel-jest",
-    // "^.+\\.svg$": "jest-svg-transformer",
     ".+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
   },
-  moduleNameMapper: {
-    "^.+\\.svg$": "jest-svg-transformer",
-  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './src/**/*.ts[x]',
+    '!**/node_modules/**',
+  ],
   modulePathIgnorePatterns: ["<rootDir>/src/__tests__/tools"],
   setupFiles: ['./setupTests.ts'],
   verbose: true,
